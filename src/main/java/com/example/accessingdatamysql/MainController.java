e7b011a8b54af36eae2d5bec19b832866f5dc064
uopsdod <uopsdod@gmail.com>
2020-04-09 20:11:58 +0800

    new

--- src/main/java/com/example/accessingdatamysql/MainController.java
+++ src/main/java/com/example/accessingdatamysql/MainController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller	// This means that this class is a Controller
 @RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
